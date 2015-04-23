2b02cc8a093d5e3f16bf27512612bdb473c5762e
scheah <sebcheah@gmail.com>
2015-04-23 01:24:27 -0700

    Fix compile errors

--- extra/instrumentation/dynamic/CountDynamicInstructionsInstrumentation.cpp
+++ extra/instrumentation/dynamic/CountDynamicInstructionsInstrumentation.cpp
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 int Record()//BasicBlock &block)
 {
-    outs() << "blah blah\n";
-    errs() << "yes\n";
     printf("Hello");
     return 0;
 }

b4ae597a634e593245b7bf2240bfa7273c7000a7
scheah <sebcheah@gmail.com>
2015-04-23 18:26:19 -0700

    Added BranchBias Instrumentation

---
+++ extra/instrumentation/branchbias/BranchBiasInstrumentation.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <map>
+
+using namespace std;
+
+
+void Record(char* function, int condition)
+{
+	if (condition) {
+	}
+}
+
+void PrintBranchBias()
+{
+	double bias = 0;
+	}
+}
+

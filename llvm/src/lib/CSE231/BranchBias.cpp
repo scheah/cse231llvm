471d8327a38841aec264df77bfcd181d1755cf34
scheah <sebcheah@gmail.com>
2015-04-24 13:32:45 -0700

    Laid out logic + formatting

--- llvm/src/lib/CSE231/BranchBias.cpp
+++ llvm/src/lib/CSE231/BranchBias.cpp
@@ -1,45 +1,57 @@
-#define DEBUG_TYPE "CountDynamicInstructions"
+#define DEBUG_TYPE "BranchBias"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
 
 namespace {
-    struct BranchBiasPass : public BasicBlockPass {
-        static char ID;
-        
-        BranchBiasPass() : BasicBlockPass(ID) {}
+	struct BranchBiasPass : public BasicBlockPass {
+		static char ID;
 
-        virtual bool runOnBasicBlock(BasicBlock &B) {
-            TerminatorInst * terminatorInst = B.getTerminator();
+		BranchBiasPass() : BasicBlockPass(ID) {}
 
-            if (terminatorInst == NULL || !TerminatorInst::classof(BranchInst)) {
-                return false;
-            }
+		virtual bool runOnBasicBlock(BasicBlock &B) {
+			Function * function = B.getParent();
+			Module * module = function->getParent();
+			Constant * recordConstant = module->getOrInsertFunction("_Z6RecordPci", FunctionType::getVoidTy(B.getContext()), PointerType::getInt8PtrTy(B.getContext()), IntegerType::get(B.getContext(),32), NULL);
+			Function * recordFunction = cast<Function>(recordConstant);
+			Constant * printConstant = module->getOrInsertFunction("_Z15PrintBranchBiasv", FunctionType::getVoidTy(B.getContext()), NULL);
+			Function * printFunction = cast<Function>(printConstant);			
 
-            BranchInst * branchInst = (BranchInst *)terminatorInst;
+			TerminatorInst * terminatorInst = B.getTerminator();
+			bool finalMainBlock = strcmp(function->getName().str().c_str(), "main") == 0 && ReturnInst::classof(terminatorInst);
+			bool branchInstPresent = BranchInst::classof(terminatorInst);
+			if (!finalMainBlock && !branchInstPresent) {
+				return false;
+			}
+			IRBuilder<> irBuilder(&B);
+			irBuilder.SetInsertPoint(terminatorInst);
 
-            if (!branchInst->isConditional()) {
-                return false;
-            }
+			if (branchInstPresent) {
+				BranchInst * branchInst = (BranchInst *)terminatorInst;
+				if (branchInst->isConditional()) {
+					Value *arg1 = irBuilder.CreateGlobalStringPtr(function->getName().str().c_str());
+					Value *arg2 = branchInst->getCondition();
+					irBuilder.CreateCall2(recordFunction, arg1, arg2);
+				}
+			}
 
-            Function * function = B.getParent();
-            Module * = function->getParent();
+			if (finalMainBlock) {
+				irBuilder.CreateCall(printFunction);
+			}
 
-            Constant * recordConstant = module->getOrInsertFunction("_Z6RecordPci", FunctionType::getVoidTy(B.getContext()), PointerType::getInt8PtrTy(B.getContext()), IntegerType::get(B.getContext(),32), NULL);
-            Function * recordFunction = cast<Function>(recordConstant);
-            Constant * printConstant = module->getOrInsertFunction("_Z15PrintBranchBiasv", FunctionType::getVoidTy(B.getContext()), NULL);
-            Function * printFunction = cast<Function>(printConstant);
-
-            IRBuilder<> irBuilder(&B);
-            irBuilder.SetInsertPoint(terminatorInst);
-
-            irBuilder.CreateCall2(recordFunction, arg1, arg2);
-
-            if (strcmp(function->getName().str().c_str(), "main") == 0 && returnStatementFound) {
-                irBuilder.CreateCall(printFunction);
-            }
-
-            return true;
-        }
-    }
+			return true;
+		}
+	};
 }
 
-
-
+char BranchBiasPass::ID = 0;
+static RegisterPass<BranchBiasPass> X("branchbias", "Branch Bias Analysis Pass");

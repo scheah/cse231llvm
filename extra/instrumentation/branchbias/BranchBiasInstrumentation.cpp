f1f2857d1c521b4998c0559d660caa6e60099054
scheah <sebcheah@gmail.com>
2015-04-24 21:21:43 -0700

    Fixed printing issues

--- extra/instrumentation/branchbias/BranchBiasInstrumentation.cpp
+++ extra/instrumentation/branchbias/BranchBiasInstrumentation.cpp
@@ -18,8 +18,8 @@ void PrintBranchBias()
 	printf("Function\tBias\tTaken\tTotal\n");
 	double bias = 0;
 	for(map<char*, map <char*, int> >::iterator iterator = dynamicBranchCounter.begin(); iterator != dynamicBranchCounter.end(); ++iterator) {
-		bias = (iterator->second)[(char*)"Taken"]/(iterator->second)[(char*)"Total"];
-		printf("%s\t%.2f\t%d\t%d\n", iterator->first, bias, (iterator->second)[(char*)"Taken"], (iterator->second)[(char*)"Total"]);
+		bias = (double)(iterator->second)[(char*)"Taken"]/(double)(iterator->second)[(char*)"Total"];
+		printf("%-15s\t%.2f\t%d\t%d\n", iterator->first, bias, (iterator->second)[(char*)"Taken"], (iterator->second)[(char*)"Total"]);
 	}
 }
 

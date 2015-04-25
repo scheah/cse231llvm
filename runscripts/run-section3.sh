8ad1dc449f1619dd49a159e3790c7ac255123a1a
scheah <sebcheah@gmail.com>
2015-04-24 23:55:23 -0700

    Update run-section3.sh

--- runscripts/run-section3.sh
+++ runscripts/run-section3.sh
@@ -9,18 +9,18 @@ LDFLAGS=
 #LDFLAGS=`llvm-config --ldflags`
 
 filename=$(basename "$1")
-filename="${filename%.*}"
-opt -load $LLVMLIB/CSE231.so -branchbias < $1/$filename.bc > $CSE231ROOT/$filename.pass.bc
+
+opt -load $LLVMLIB/CSE231.so -branchbias < $1/$filename.bc > $1/$filename.pass.bc
 ## compile the instrumentation module to bitcode
 clang $CPPFLAGS -O0 -emit-llvm -c $INSTRUMENTATION/branchbias/BranchBiasInstrumentation.cpp -o $INSTRUMENTATION/branchbias/BranchBiasInstrumentation.bc
 
 ## link instrumentation module
-llvm-link $CSE231ROOT/$filename.pass.bc $INSTRUMENTATION/branchbias/BranchBiasInstrumentation.bc -o $INSTRUMENTATION/branchbias/$filename.linked.bc
+llvm-link $1/$filename.pass.bc $INSTRUMENTATION/branchbias/BranchBiasInstrumentation.bc -o $INSTRUMENTATION/branchbias/$filename.linked.bc
 
 ## compile to native object file
 llc -filetype=obj $INSTRUMENTATION/branchbias/$filename.linked.bc -o=$INSTRUMENTATION/branchbias/$filename.o
 
-	## generate native executable
+## generate native executable
 g++ $INSTRUMENTATION/branchbias/$filename.o $LLVMLIBS $LDFLAGS -o $INSTRUMENTATION/branchbias/$filename.exe
 
 $INSTRUMENTATION/branchbias/$filename.exe > $OUTPUTLOGS/$filename.branchbias.log

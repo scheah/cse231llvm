746e0aa340a74c6334ecb7f246a3ba8cb0ab9090
scheah <sebcheah@gmail.com>
2015-04-27 15:43:17 -0700

    Format print

--- extra/instrumentation/branchbias/BranchBiasInstrumentation.cpp
+++ extra/instrumentation/branchbias/BranchBiasInstrumentation.cpp
@@ -15,11 +15,10 @@ void Record(char* function, int condition)
 
 void PrintBranchBias()
 {
-	printf("Function\tBias\tTaken\tTotal\n");
+	printf("%-40s\tBias\tTaken\tTotal\n", "Function");
 	double bias = 0;
 	for(map<char*, map <char*, int> >::iterator iterator = dynamicBranchCounter.begin(); iterator != dynamicBranchCounter.end(); ++iterator) {
 		bias = (double)(iterator->second)[(char*)"Taken"]/(double)(iterator->second)[(char*)"Total"];
-		printf("%-15s\t%.2f\t%d\t%d\n", iterator->first, bias, (iterator->second)[(char*)"Taken"], (iterator->second)[(char*)"Total"]);
+		printf("%-40s\t%.2f\t%d\t%d\n", iterator->first, bias, (iterator->second)[(char*)"Taken"], (iterator->second)[(char*)"Total"]);
 	}
 }
-
--- extra/instrumentation/dynamic/CountDynamicInstructionsInstrumentation.cpp
+++ extra/instrumentation/dynamic/CountDynamicInstructionsInstrumentation.cpp
@@ -14,9 +14,8 @@ void PrintInstructionCount()
 {
 	int sum = 0;
 	for(map<char*, int>::iterator iterator = dynamicInstCounter.begin(); iterator != dynamicInstCounter.end(); ++iterator) {
-		printf("%s\t%d\n", iterator->first, iterator->second);
+		printf("%-20s\t%d\n", iterator->first, iterator->second);
 		sum += iterator->second;
 	}
-	printf("%s\t%d\n", "TOTAL", sum);
+	printf("%-20s\t%d\n", "TOTAL", sum);
 }
-
